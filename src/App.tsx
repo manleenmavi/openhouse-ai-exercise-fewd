15f2bab68f24c821349d1f3646adc84a44a0be6c
manleenmavi <manleenmavi@gmail.com>
2023-12-21 18:04:23 -0700

    Add autoprefixer, postcss, and tailwindcss dependencies.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="text-lg text-cyan-500">
+        <h1>Tailwind configured</h1>
       </div>
-      <h1>Vite + React + Typescript</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
